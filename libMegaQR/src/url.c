4a841ac545806c8567571d22347edbf1a1ad275e
paspallas <paspallasdev@gmail.com>
2026-05-18 00:52:34 +0200

    fix typo

--- libMegaQR/src/url.c
+++ libMegaQR/src/url.c
@@ -5,5 +5,5 @@
 #if MEGAQR_DEBUG == 1
     const char *url = "aHR0cHM6Ly9sZWFkZXJib2FyZC5wbGF5b25yZXRyby5jb20vYXBpL3Qv";
 #else
-    const char *ulr = "aHR0cHM6Ly9sZWFkZXJib2FyZC5wbGF5b25yZXRyby5jb20vYXBpL3Mv";
+    const char *url = "aHR0cHM6Ly9sZWFkZXJib2FyZC5wbGF5b25yZXRyby5jb20vYXBpL3Mv";
 #endif
